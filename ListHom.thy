b4e838ed9942bf0c5db898733d8fd3e3ad858ce1
Johannes Sauer <jtsauer@gmail.com>
2013-12-28 17:19:34 +0000

    cosmetic changes, began proofs: hom_uniq, hom_impl_map

--- ListHom.thy
+++ ListHom.thy
@@ -8,72 +8,72 @@ begin
   J. Gibbons, 1995".
 *)
 
-definition hom :: "['a list \<Rightarrow> 'b, 'b \<Rightarrow> 'b \<Rightarrow> 'b] \<Rightarrow> bool" 
-  where "hom h b = (\<forall> xs ys. b (h xs) (h ys) = h (xs @ ys))"
+definition hom' :: "['a list \<Rightarrow> 'b, 'b \<Rightarrow> 'b \<Rightarrow> 'b] \<Rightarrow> bool" 
+  where "hom' h b \<longleftrightarrow> (\<forall> xs ys. b (h xs) (h ys) = h (xs @ ys))"
 
-lemma hom_b_lunit:
-  assumes h' : "hom h b" 
+lemma hom'_b_lunit:
+  assumes h' : "hom' h b" 
   and     x' : "x = h xs" 
   and     e' : "e = h []"
   shows   "b e x = x"
   proof - 
-    from e'
-    have "b e x = b (h []) x" by simp
-    also from x'
-    have "... = b (h []) (h xs)" by simp
-    also from h' 
-    have "... = h ([] @ xs)" by (simp only: hom_def)
-    also have "... = h xs" by simp
-    also from x'
-    have "... = x" by simp
+    have "b e x = b (h []) x" 
+      using e'  by simp
+    also have "... = b (h []) (h xs)" 
+      using x' by simp
+    also have "... = h ([] @ xs)" 
+      using h' by (simp only: hom'_def)
+    also have "... = h xs" 
+      by simp
+    also have "... = x" 
+      using x' by simp
     finally show ?thesis .
     qed
 
-lemma hom_b_runit:
-  assumes h' : "hom h b" 
+
+lemma hom'_b_runit:
+  assumes h' : "hom' h b" 
   and     x' : "x = h xs" 
   and     e' : "e = h []"
   shows   "b x e = x"
-    proof - 
-    from e'
-    have "b x e = b x (h [])" by simp
-    also from x'
-    have "... = b (h xs) (h [])" by simp
-    also from h' 
-    have "... = h (xs @ [])" by (simp only: hom_def)
-    also have "... = h xs" by simp
-    also from x'
-    have "... = x" by simp
+  proof - 
+    have "b x e = b x (h [])" 
+      using e' by simp
+    also have "... = b (h xs) (h [])" 
+      using x' by simp
+    also have "... = h (xs @ [])" 
+      using h' by (simp only: hom'_def)
+    also have "... = h xs" 
+      by simp
+    also have "... = x" 
+      using x' by simp
     finally show ?thesis .
     qed
 
-
-lemma hom_b_assoc:
-  assumes h' : "hom h b" 
+lemma hom'_b_assoc:
+  assumes h' : "hom' h b" 
   and     x' : "x = h xs" 
   and     y' : "y = h ys" 
   and     z' : "z = h zs"
   shows   "b (b x y) z = b x (b y z)"
   proof -
     note x' y' h' then
-    have "b (b x y) z = b (h (xs @ ys)) z" by (simp only: hom_def)
+    have "b (b x y) z = b (h (xs @ ys)) z" by (simp only: hom'_def)
     also from z' 
     have "... = b (h (xs @ ys)) (h zs)" by simp
     also from h'
-    have "... = h (xs @ ys @ zs)" by (simp add: hom_def)
+    have "... = h (xs @ ys @ zs)" by (simp add: hom'_def)
     also from h'
-    have "... = b (h xs) (h (ys @ zs))" by (simp add: hom_def)
+    have "... = b (h xs) (h (ys @ zs))" by (simp add: hom'_def)
     also from x' 
     have "... = b x (h (ys @ zs))" by simp
     also note y' z' h' then
-    have "... = b x (b y z)" by (simp only: hom_def)
+    have "... = b x (b y z)" by (simp only: hom'_def)
     finally show ?thesis .
     qed
 
 fun wrap :: "'a \<Rightarrow> 'a list" 
   where "wrap x = [x]"
-
-
 
 lemma wrap_inj: 
   shows "inj wrap"
@@ -99,31 +99,54 @@ lemma wrap_inj:
     qed
 *)
 
-definition hom_uniq :: "['a list \<Rightarrow> 'b, 'b \<Rightarrow> 'b \<Rightarrow> 'b, 'a \<Rightarrow> 'b] \<Rightarrow> bool"
-  where "hom_uniq h b f = (hom h b \<and> h \<circ> wrap = f)"
+definition hom :: "['a list \<Rightarrow> 'b, 'b \<Rightarrow> 'b \<Rightarrow> 'b, 'a \<Rightarrow> 'b] \<Rightarrow> bool"
+  where "hom h b f \<longleftrightarrow> (hom' h b) \<and> (h \<circ> wrap = f)"
 
 lemma hom_uniq:
-  assumes  h' : "hom_uniq h b f"
-  and      g' : "hom_uniq g b f"
+  assumes  h' : "hom h b f"
+  and      g' : "hom g b f"
   shows    "h = g"
   proof -
     from h'
     have "h \<circ> wrap = f" 
-      by (simp only: hom_uniq_def)
+      by (simp only: hom_def)
     also from g'
     have "... = g \<circ> wrap" 
-      by (simp only: hom_uniq_def)
+      by (simp add: hom_def)
     finally 
     have "h \<circ> wrap = g \<circ> wrap" .
-    then have "h = g" by (simp add: wrap_inj) 
+    then have "h = g" oops
 (* needs proof of left cancellation of inj funs *)
-      
-lemma hom_map:
-  assumes "hom_uni m @ (f \<circ> wrap)"
-  shows   "m = map f"
+     
+lemma hom_impl_map:
+  assumes  "hom m (op @) (wrap \<circ> f)"
+  shows    "m = map f"
+  proof 
+    fix xs
+    show "m xs = map f xs"
+      proof (induct xs)
+        case Nil 
+        thus ?case          
+          proof -
+            have "m [] = m ([] @ [])" 
+              by simp 
+            also 
+            have "... = (m []) @ (m [])"
+            proof - (* this might be a candidate for an accessor *)
+              from assms
+              have "hom' m (op @)" 
+                unfolding hom_def
+                by (rule conjE)
+              thus ?thesis
+                unfolding hom'_def
+                by simp
+              qed 
+              thus ?thesis 
+                by simp
+          qed
 
 theorem fst_hom:
-  assumes h' : "hom_uni h b f"
-  shows   "\<exists> r. hom_uni r b id \<and> h = r \<circ> map f"
+  assumes h' : "hom h b f"
+  shows   "\<exists> r. hom r b id \<and> h = r \<circ> map f"
           
 end
