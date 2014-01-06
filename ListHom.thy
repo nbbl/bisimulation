327da7753b259577201c9eb281b0dd8b0986f4bf
Johannes Sauer <jtsauer@gmail.com>
2014-01-06 23:57:02 +0000

    new version of hom'_assoc, continued proof of hom_app_eq_map, minor (cosmetic) changes

--- ListHom.thy
+++ ListHom.thy
@@ -11,57 +11,47 @@ begin
 definition hom' :: "['a list \<Rightarrow> 'b, 'b \<Rightarrow> 'b \<Rightarrow> 'b] \<Rightarrow> bool" 
   where "hom' h b \<longleftrightarrow> (\<forall> xs ys. b (h xs) (h ys) = h (xs @ ys))"
 
-lemma hom'_lunit:
+lemma hom'_l_id:
   assumes "hom' h b" 
-  shows   "\<forall> x \<in> range h. b (h []) x = x"
+  shows   "\<forall> xs \<in> range h. b (h []) xs = xs"
   proof
-    fix x 
-    assume "x \<in> range h"
-    then obtain y where * : "x = h y" ..
-    then have "b (h []) x = b (h []) (h y)" by simp
-    also have "... = h y" using assms by (simp add : hom'_def)
-    also have "... = x" using * by simp
-    finally show "b (h []) x = x" .
-    qed
+    fix xs 
+    assume "xs \<in> range h"
+    then obtain xs' where * : "xs = h xs'" ..
+    then have "b (h []) xs = b (h []) (h xs')" by simp
+    also have "... = h xs'" using assms by (simp add : hom'_def)
+    also have "... = xs" using * by simp
+    finally show "b (h []) xs = xs" .
+  qed
 
-lemma hom'_runit:
+lemma hom'_r_id:
   assumes "hom' h b" 
-  shows   "\<forall> x \<in> range h. b x (h []) = x"
+  shows   "\<forall> xs \<in> range h. b xs (h []) = xs"
   proof 
-    fix x 
-    assume "x \<in> range h"
-    then obtain y where * : "x = h y" ..
-    then have "b x (h []) = b (h y) (h [])" by simp
-    also have "... = h y" using assms by (simp add : hom'_def)
-    also have "... = x" using * by simp
-    finally show "b x (h []) = x" .
-    qed
+    fix xs 
+    assume "xs \<in> range h"
+    then obtain xs' where * : "xs = h xs'" ..
+    then have "b xs (h []) = b (h xs') (h [])" by simp
+    also have "... = h xs'" using assms by (simp add : hom'_def)
+    also have "... = xs" using * by simp
+    finally show "b xs (h []) = xs" .
+  qed
 
-(*
-should be modelled with universal quantifier like the units:
-
-lemma hom'_assoc: 
-  assumes h' : "hom' h b" 
-  and     x' : "x = h xs" 
-  and     y' : "y = h ys" 
-  and     z' : "z = h zs"
-  shows   "b (b x y) z = b x (b y z)"
-  proof -
-    note x' y' h' then
-    have "b (b x y) z = b (h (xs @ ys)) z" by (simp only: hom'_def)
-    also from z' 
-    have "... = b (h (xs @ ys)) (h zs)" by simp
-    also from h'
-    have "... = h (xs @ ys @ zs)" by (simp add: hom'_def)
-    also from h'
-    have "... = b (h xs) (h (ys @ zs))" by (simp add: hom'_def)
-    also from x' 
-    have "... = b x (h (ys @ zs))" by simp
-    also note y' z' h' then
-    have "... = b x (b y z)" by (simp only: hom'_def)
-    finally show ?thesis .
-    qed
-*)
+lemma hom'_assoc:
+  assumes "hom' h b"
+  shows   "\<forall> xs \<in> range h. \<forall> ys \<in> range h. \<forall> zs \<in> range h. 
+           b xs (b ys zs) = b (b xs ys) zs"
+  proof - 
+    fix xs ys zs
+    assume "xs \<in> range h" 
+    then obtain xs' where 1 : "xs = h xs'" ..
+    assume "ys \<in> range h" 
+    then obtain ys' where 2 : "ys = h ys'" ..
+    assume "zs \<in> range h" 
+    then obtain zs' where 3 : "zs = h zs'" ..
+    from 1 2 3 and assms
+    have "b xs (b ys zs) = b (h xs') (h (ys' @ zs'))" by (simp only: hom'_def)
+    also have  "... = h (xs' @ ys' @ zs')" oops
 
 fun wrap :: "'a \<Rightarrow> 'a list" 
   where "wrap x = [x]"
@@ -99,57 +89,57 @@ lemma hom_impl_hom':
   proof -
     from assms
     show ?thesis
-      unfolding hom_def
-      by (rule conjE)
-    qed
+      unfolding hom_def by (rule conjE)
+  qed
 
 lemma hom_uniq:
-  assumes  h' : "hom h b f e"
-  and      g' : "hom g b f e"
+  assumes  hom_h : "hom h b f e"
+  and      hom_g : "hom g b f e"
   shows    "h = g"
   proof 
     fix xs 
     show "h xs = g xs"
     proof (induct xs)
-      case Nil
-      thus ?case
-        using h' and g'
-        by (simp add: hom_def)
+      case Nil thus ?case
+        using hom_h and hom_g by (simp add: hom_def)
     next
-      case (Cons x xs')
-      thus ?case
+      case (Cons x xs') thus ?case
       proof -
         assume "h xs' = g xs'"
         moreover have "h [x] = g [x]"
         proof - 
           have "f x = f x" by simp
           also have "(h \<circ> wrap) x = (g \<circ> wrap) x"
-            using h' and g'
-            by (simp only: hom_def)
+            using hom_h and hom_g by (simp only: hom_def)
           thus ?thesis by simp
         qed
         ultimately have "b (h [x]) (h xs') = b (g [x]) (g xs')" by simp
-        thus "h (x # xs') = g (x # xs')"  oops
+        with hom_impl_hom' [OF hom_h] and hom_impl_hom' [OF hom_g]
+        show "h (x # xs') = g (x # xs')" by (simp add: hom'_def)
+      qed
+    qed
+  qed
         
-lemma hom_impl_map:
-  assumes  "hom m (op @) (wrap \<circ> f) e"
+lemma hom_app_eq_map:
+  assumes  "hom m (op @) (wrap \<circ> f) []"
   shows    "m = map f"
   proof 
-    fix xs
-    show "m xs = map f xs"
+    fix xs show "m xs = map f xs"
       proof (induct xs)
-        case Nil 
-        thus ?case          
+        case Nil thus ?case          
         proof -
-          have "m [] = m ([] @ [])" 
-            by simp 
-          also 
-          have "... = (m []) @ (m [])"
-            oops
+          from assms have "m [] = []" by (simp only: hom_def)
+          also have "... = map f []" by simp 
+          finally show "m [] = map f []" .
+        qed
+      next
+        case Cons thus ?case
+        proof -
+          oops
 
 theorem fst_hom:
-  assumes h' : "hom h b f"
-  shows   "\<exists> r. hom r b id \<and> h = r \<circ> map f"
+  assumes "hom h b f e"
+  shows   "\<exists> r. (hom r b id e) \<and> (h = r \<circ> map f)"
   oops
 
 end
